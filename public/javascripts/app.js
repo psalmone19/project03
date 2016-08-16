db8f15f729c2b8a6ac974df5065a1362fa75c22f
Sally Kang <SallyKang@Sallys-MacBook-Pro.local>
2016-08-16 16:39:12 -0700

    Added canvas clear and chat timestamp

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,19 @@
 // socket by Egan & Antonio
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-   // get our connection to the socket.io server
-   var socket = io();
-//    console.log(socket);
+      // get our connection to the socket.io server
+      
+    var socket = io();
 
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<i class="fa fa-commenting-o" aria-hidden="true" id="upMsg">').text(msg));
-  })
+    $('form').submit(function () {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', function (msg) {
+        var momentTimestamp = moment.utc(msg.timestamp);
+        console.log(momentTimestamp)
+        $('#messages').append($('<i class="fa fa-commenting-o" aria-hidden="true" id="upMsg">').text(' '+ msg +' '+ momentTimestamp.local().format('h:mm a')));
+    })
+
 });
